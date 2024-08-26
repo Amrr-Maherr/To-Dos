4c9808c5109887f6a116a9b5e44193316c939119
Amr maher <amrr.maherr24@gmail.com>
2024-08-27 02:19:05 +0300

    Enhance To-Do List with Task Creation, Input Validation, and Animations
    - Improved HTML structure by adding a form with Bootstrap styling for task input.
    - Added "Add Task" button with full-width styling and linked it to task creation functionality.
    - Implemented `createEle` function to handle task addition with input validation.
    - Included animation classes from Animate.css for task entries and button interactions.
    - Added a favicon to the application and linked external CSS for Bootstrap and Animate.css.
    - Included console logs for debugging and verifying element selections and functionality.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 let inputText = document.querySelector(".form-control");
-console.log(inputText);
 let inputSub = document.querySelector('.btn');
-console.log(inputSub);
+let ulList = document.querySelector('.list');
 
+function createEle() {
+    if (inputText.value == '') {
+        alert('Please Inter A Task')
+    } else {
+        let task = document.createElement("li");
+        let taskText = document.createTextNode(`${inputText.value}`);
+        task.appendChild(taskText);
+        task.classList.add("animate__animated", "animate__backInLeft");
+        ulList.appendChild(task);
+        console.log(ulList);
+        inputText.value = '';
+        inputSub.classList.add("animate__animated", "animate__pulse");
+    }
+}
+inputSub.onclick = function () {
+    createEle();
+}
